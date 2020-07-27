1de7e55427f1091c39d96ea1a4237fbe063e2209
Daniel Rodriguez <drg809@gmail.com>
2020-07-27 23:54:12 +0200

    new model added

---
+++ src/models/summonerEntries.ts
@@ -0,0 +1,16 @@
+import * as mongoose from "mongoose";
+
+const summonersEntriesSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId, required: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    entrie: { type: String, unique : true, required : true, dropDups: true }
+});
+
+export interface ISummonerEntries extends mongoose.Document {
+  userId?: string,
+  entrie?: string
+};
+
+const summonersEntriesModel = mongoose.model<ISummonerEntries>('summonersEntries', summonersEntriesSchema);
+
+export { summonersEntriesModel }
