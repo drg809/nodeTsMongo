d4bf2e88a36bfb7d06d21dd41aa353e542117efe
Daniel Rodriguez <drg809@gmail.com>
2020-07-28 01:26:20 +0200

    match embbed in entrie

--- src/models/summonerEntries.ts
+++ src/models/summonerEntries.ts
@@ -7,8 +7,32 @@ const summonersEntriesSchema = new mongoose.Schema({
 });
 
 export interface ISummonerEntries extends mongoose.Document {
-  userId?: string,
-  entrie?: string
+    userId?: string,
+    entrie?: {
+        userId?: string,
+        entrie?: string,
+        data?: { metadata: object, info: {
+            game_datetime: number,
+            game_length: number,
+            game_variation: string,
+            game_version: string,
+            participants: {
+                companion: object,
+                gold_left: number,
+                last_round: number,
+                level: number,
+                placement: number,
+                players_eliminated: number,
+                puuid: string,
+                time_eliminated: number,
+                total_damage_to_players: number,
+                traits: object,
+                units: object
+            },
+            queue_id: number,
+            tft_set_number: number
+        } }
+    }
 };
 
 const summonersEntriesModel = mongoose.model<ISummonerEntries>('summonersEntries', summonersEntriesSchema);
