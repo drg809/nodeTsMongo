8cb450473c3bc18481b37e8a8701d7e0d3c56f9f
Daniel Rodriguez <drg809@gmail.com>
2020-07-30 08:05:37 +0200

    retries array created

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -3,4 +3,9 @@ require('dotenv').config();
 export default {
     jwtSecret: process.env.JWT_TOKEN_KEY || "testMongoBasics",
     port: process.env.PORT || 8080,
+    retries: [
+        'createSum',
+        'updateSumHis',
+        'update'
+    ],
 };
