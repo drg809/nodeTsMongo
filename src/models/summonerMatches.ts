a694d336a2fe4c7ad01b988dd3b816a85f751279
Daniel Rodriguez <drg809@gmail.com>
2020-08-02 17:33:49 +0200

    match model expanded

--- src/models/summonerMatches.ts
+++ src/models/summonerMatches.ts
@@ -29,7 +29,18 @@ const summonersMatchesSchema = new mongoose.Schema({
         game_length: Number,
         game_variation: String,
         game_version: String,
-        participants: Object,
+        participants: [{
+          companion: Object,
+          gold_left: Number,
+          last_round: Number,
+          level: Number,
+          placement: Number,
+          players_eliminated: Number,
+          puuid: String,
+          time_eliminated: Number,
+          total_damage_to_players: Number,
+          name: String
+        }],
         queue_id: Number,
         tft_set_number: Number
       }
@@ -50,7 +61,18 @@ export interface ISummonerMatches extends mongoose.Document {
           game_length: number,
           game_variation: string,
           game_version: string,
-          participants: object,
+          participants: [{
+            companion: object,
+            gold_left: number,
+            last_round: number,
+            level: number,
+            placement: number,
+            players_eliminated: number,
+            puuid: string,
+            time_eliminated: number,
+            total_damage_to_players: number,
+            name?: string
+          }],
           queue_id: number,
           tft_set_number: number
       }
