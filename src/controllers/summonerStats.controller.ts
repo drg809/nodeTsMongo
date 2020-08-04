7e7ca5c261d4d5dcc9eb412a9233c92fcf44ed2c
Daniel Rodriguez <drg809@gmail.com>
2020-08-05 01:51:14 +0200

    new summonerStats controller

---
+++ src/controllers/summonerStats.controller.ts
@@ -0,0 +1,74 @@
+import { Route, Get, Controller, Post, BodyProp, Put, Delete, SuccessResponse } from 'tsoa';
+import { summonerStatsModel, ISummonerStats} from '../models/summonerStats';
+
+@Route('/')
+export class summonerStatsController extends Controller {
+    @Get('/summonerStats')
+    public async getAll() : Promise<any[]> {
+        try {
+            let items: any[] = await summonerStatsModel.find({});
+            items = items.map((item) => { return {
+                id: item._id,
+                userId: item.userId,
+                action: item.action,
+                summonerStats: item.summonerStats,
+                first: item.first,
+                last: item.last
+              }
+            });
+            return items;
+        } catch (err) {
+            this.setStatus(500);
+            console.error('Caught error', err);
+        }
+    }
+
+    @Get('/summonerStats/{id}')
+    public async getOne(id: string) : Promise<any[]> {
+        try {
+            let item: any = await summonerStatsModel.findOne({_id: id});
+            return item;
+        } catch (err) {
+            this.setStatus(500);
+            console.error('Caught error', err);
+        }
+    }
+
+    @Post('/summonerStats')
+    public async create(@BodyProp('userId') userId: string,
+     @BodyProp('action') action: string,
+     @BodyProp('summonerStats') summonerStats: string,
+     @BodyProp('first') first: Date,
+     @BodyProp('last') last: Date ): Promise<any> {
+        const item = new summonerStatsModel({
+          userId: userId,
+          action: action,
+          summonerStats: summonerStats,
+          first: first,
+          last: last
+        });
+        await item.save();
+        return item;
+    }
+
+    @Put('/summonerStats/{id}')
+    public async update(id: string,
+      @BodyProp('userId') userId: string,
+      @BodyProp('action') action: string,
+      @BodyProp('summonerStats') summonerStats: number,
+      @BodyProp('first') first: Date,
+      @BodyProp('last') last: Date ): Promise<void> {
+        await summonerStatsModel.findOneAndUpdate({_id: id}, { $set: {
+          userId: userId,
+          action: action,
+          summonerStats: summonerStats,
+          first: first,
+          last: last
+        } } );
+    }
+
+    @Delete('/summonerStats/{id}')
+    public async remove(id: string): Promise<void> {
+        await summonerStatsModel.findByIdAndRemove(id);
+    }
+}
