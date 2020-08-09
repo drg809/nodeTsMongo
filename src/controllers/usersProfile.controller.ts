276028c9bf674f6b8d78c3480d74c2809ad8d6e9
Daniel Rodriguez <drg809@gmail.com>
2020-08-09 08:18:43 +0200

    user profile controller

---
+++ src/controllers/usersProfile.controller.ts
@@ -0,0 +1,68 @@
+import { Route, Get, Controller, Post, BodyProp, Put, Delete, SuccessResponse } from 'tsoa';
+import { usersProfileModel, IUserProfile} from '../models/usersProfile';
+
+@Route('/')
+export class usersProfileController extends Controller {
+    @Get('/profile')
+    public async getAll() : Promise<any[]> {
+        try {
+            let items: any[] = await usersProfileModel.find({deletedAt: { $eq: null }});
+            items = items.map((item) => { return {
+                id: item._id,
+                name: item.name,
+              }
+            });
+            return items;
+        } catch (err) {
+            this.setStatus(500);
+            console.error('Caught error', err);
+        }
+    }
+
+    @Get('/profile/{id}')
+    public async getOne(id: string) : Promise<any[]> {
+        try {
+            let item: any = await usersProfileModel.findOne({_id: id, deletedAt: { $eq: null }});
+            return item;
+        } catch (err) {
+            this.setStatus(500);
+            console.error('Caught error', err);
+        }
+    }
+
+    @Post('/profile')
+    public async create(@BodyProp('name') name: string,
+     @BodyProp('lastname') lastname: string,
+     @BodyProp('phone') phone: string,
+     @BodyProp('country') country: number): Promise<any> {
+        const item = new usersProfileModel({
+          name: name,
+          lastname: lastname,
+          phone: phone,
+          country: country
+        });
+        await item.save();
+        return item;
+    }
+
+    @Put('/profile/{id}')
+    public async update(id: string,
+     @BodyProp('name') name: string,
+     @BodyProp('lastname') lastname: string,
+     @BodyProp('phone') phone: string,
+     @BodyProp('country') country: string): Promise<void> {
+        await usersProfileModel.findOneAndUpdate({_id: id, deletedAt: { $eq: null }}, { $set: {
+          name: name,
+          lastname: lastname,
+          phone: phone,
+          country: country
+        } } );
+    }
+
+    @Delete('/profile/{id}')
+    public async remove(id: string): Promise<void> {
+        await usersProfileModel.findOneAndUpdate({_id: id, deletedAt: { $eq: null }}, { $set: {
+          deletedAt: new Date()
+        } } );
+    }
+}
