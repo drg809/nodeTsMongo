673f61b5f30be9f2e026e0b3d01e25aa210053bc
Daniel Rodriguez <drg809@gmail.com>
2020-08-13 07:40:51 +0200

    matchnotes model created

---
+++ src/models/matchNotes.ts
@@ -0,0 +1,18 @@
+import * as mongoose from "mongoose";
+
+const matchNotesSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId, required: false },
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    entrieId: { type: String, required : true, dropDups: true },
+    text: { type: String, required : true, dropDups: true }
+});
+
+export interface IMatchNotes extends mongoose.Document {
+    userId?: string,
+    entrieId?: string,
+    text?: string
+};
+
+const matchNotesModel = mongoose.model<IMatchNotes>('matchNotes', matchNotesSchema);
+
+export { matchNotesModel }
