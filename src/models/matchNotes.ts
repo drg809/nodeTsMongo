81b92dc50c12304d580d36fe9087da87cb9b3acb
Daniel Rodriguez <drg809@gmail.com>
2020-08-16 06:00:27 +0200

    visible field added to match notes

--- src/models/matchNotes.ts
+++ src/models/matchNotes.ts
@@ -4,13 +4,15 @@ const matchNotesSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId, required: false },
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     entrieId: { type: String, required : true, dropDups: true },
-    text: { type: String, required : true, dropDups: true }
+    text: { type: String, required : true, dropDups: true },
+    visible: { type: Boolean, required : true, dropDups: true }
 });
 
 export interface IMatchNotes extends mongoose.Document {
     userId?: string,
     entrieId?: string,
-    text?: string
+    text?: string,
+    visible?: boolean
 };
 
 const matchNotesModel = mongoose.model<IMatchNotes>('matchNotes', matchNotesSchema);
