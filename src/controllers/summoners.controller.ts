00e046e34ce31b0cfbc5f2d3838e3df51fa029c8
Daniel Rodriguez <drg809@gmail.com>
2020-08-17 06:32:39 +0200

    save summonerId in entries and match

--- src/controllers/summoners.controller.ts
+++ src/controllers/summoners.controller.ts
@@ -138,149 +138,154 @@ export class summonersController extends Controller {
 
     @Post('/summoners/stats')
     public async getSummonerStats(req: Request) {
-        let top1: number = 0; let top2: number = 0; let top3: number = 0; let top4: number = 0; let top5: number = 0; let top6: number = 0; let top7: number = 0; let top8: number = 0;let countsT1 = {};let traitsArrayT4S3 = [];let countsT4S3 = {};
-        let champCountTop1 = {};let champCountTotal = {};let champCount = {};let unitsArrayTop1 = [];let unitsArrayTotal = [];let traitsArrayTotal = [];let traitsArrayT4 = [];let traitsArrayT4S1 = [];let traitsArrayT4S2 = [];let countsTotal = {};
-        let countsTop4 = {};let counts = {};let winRateGalaxie = {top1:{},top2:{},top3:{},top4:{},top5:{},top6:{},top7:{},top8:{},top:[]};let w = [];let unitsArray = [];let traitsArrayT1 = [];let countsT4 = {};let countsT4S1 = {};let countsT4S2 = {};
-        const auth = usersController.getTokenPayload(req);
-        let sum: ISummoner = await summonersModel.findOne({userId: auth.id, main: true});
-        let stats: ISummonerStats = await summonersStatsModel.findOne({summonerName:{ $regex : new RegExp(sum.summonerName, "i") }});
-        let matches: ISummonerMatchesDetails[] = await summonersMatchesDetailsModel.find({userId: auth.id});
-        stats.positions = {top1: top1,top2: top2,top3: top3,top4: top4,top5: top5,top6: top6,top7: top7,top8: top8, total: matches.length + 1};
-        for (const x of matches) {
-          switch(x.data.placement){
-            case 1:
-              stats.positions.top1++;
-              winRateGalaxie.top1[x.data.game_variation] = (winRateGalaxie.top1[x.data.game_variation] || 0)+1;
-              winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
-              for (const u of x.data.units) {
-                unitsArrayTop1.push(u);
+        try {
+            let top1: number = 0; let top2: number = 0; let top3: number = 0; let top4: number = 0; let top5: number = 0; let top6: number = 0; let top7: number = 0; let top8: number = 0;let countsT1 = {};let traitsArrayT4S3 = [];let countsT4S3 = {};
+            let champCountTop1 = {};let champCountTotal = {};let champCount = {};let unitsArrayTop1 = [];let unitsArrayTotal = [];let traitsArrayTotal = [];let traitsArrayT4 = [];let traitsArrayT4S1 = [];let traitsArrayT4S2 = [];let countsTotal = {};
+            let countsTop4 = {};let counts = {};let winRateGalaxie = {top1:{},top2:{},top3:{},top4:{},top5:{},top6:{},top7:{},top8:{},top:[]};let w = [];let unitsArray = [];let traitsArrayT1 = [];let countsT4 = {};let countsT4S1 = {};let countsT4S2 = {};
+            const auth = usersController.getTokenPayload(req);
+            let sum: ISummoner = await summonersModel.findOne({userId: auth.id, main: true});
+            let stats: ISummonerStats = await summonersStatsModel.findOne({summonerName:{ $regex : new RegExp(sum.summonerName, "i") }});
+            let matches: ISummonerMatchesDetails[] = await summonersMatchesDetailsModel.find({userId: auth.id});
+            stats.positions = {top1: top1,top2: top2,top3: top3,top4: top4,top5: top5,top6: top6,top7: top7,top8: top8, total: matches.length + 1};
+            for (const x of matches) {
+              switch(x.data.placement){
+                case 1:
+                  stats.positions.top1++;
+                  winRateGalaxie.top1[x.data.game_variation] = (winRateGalaxie.top1[x.data.game_variation] || 0)+1;
+                  winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
+                  for (const u of x.data.units) {
+                    unitsArrayTop1.push(u);
+                  }
+                  for (const u of x.data.traits) {
+                    if (u.style > 0) {
+                      traitsArrayT1.push(u);
+                    }
+                  }
+                  break;
+                case 2:
+                  stats.positions.top2++;
+                  winRateGalaxie.top2[x.data.game_variation] = (winRateGalaxie.top2[x.data.game_variation] || 0)+1;
+                  winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
+                  break;
+                case 3:
+                  stats.positions.top3++;
+                  winRateGalaxie.top3[x.data.game_variation] = (winRateGalaxie.top3[x.data.game_variation] || 0)+1;
+                  winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
+                  break;
+                case 4:
+                  stats.positions.top4++;
+                  winRateGalaxie.top3[x.data.game_variation] = (winRateGalaxie.top3[x.data.game_variation] || 0)+1;
+                  winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
+                  break;
+                case 5:
+                  stats.positions.top5++;
+                  winRateGalaxie.top5[x.data.game_variation] = (winRateGalaxie.top5[x.data.game_variation] || 0)+1;
+                  break;
+                case 6:
+                  stats.positions.top6++;
+                  winRateGalaxie.top6[x.data.game_variation] = (winRateGalaxie.top6[x.data.game_variation] || 0)+1;
+                  break;
+                case 7:
+                  stats.positions.top7++;
+                  winRateGalaxie.top7[x.data.game_variation] = (winRateGalaxie.top7[x.data.game_variation] || 0)+1;
+                  break;
+                default:
+                  stats.positions.top8++;
+                  winRateGalaxie.top8[x.data.game_variation] = (winRateGalaxie.top8[x.data.game_variation] || 0)+1;
               }
-              for (const u of x.data.traits) {
-                if (u.style > 0) {
-                  traitsArrayT1.push(u);
+
+              if(x.data.placement < 5) {
+                countsTop4[x.data.game_variation] = (countsTop4[x.data.game_variation] || 0)+1;
+                for (const t of x.data.units) {
+                  unitsArray.push(t);
+                }
+                for (const s of x.data.traits) {
+                  if (s.style > 0) {
+                    traitsArrayT4.push(s);
+                  }
+                  if(s.style === 1) {
+                    traitsArrayT4S1.push(s);
+                  } else if (s.style === 2) {
+                    traitsArrayT4S2.push(s);
+                  } else if (s.style === 3) {
+                    traitsArrayT4S3.push(s);
+                  }
                 }
               }
-              break;
-            case 2:
-              stats.positions.top2++;
-              winRateGalaxie.top2[x.data.game_variation] = (winRateGalaxie.top2[x.data.game_variation] || 0)+1;
-              winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
-              break;
-            case 3:
-              stats.positions.top3++;
-              winRateGalaxie.top3[x.data.game_variation] = (winRateGalaxie.top3[x.data.game_variation] || 0)+1;
-              winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
-              break;
-            case 4:
-              stats.positions.top4++;
-              winRateGalaxie.top3[x.data.game_variation] = (winRateGalaxie.top3[x.data.game_variation] || 0)+1;
-              winRateGalaxie.top[x.data.game_variation] = (winRateGalaxie.top[x.data.game_variation] || 0)+1;
-              break;
-            case 5:
-              stats.positions.top5++;
-              winRateGalaxie.top5[x.data.game_variation] = (winRateGalaxie.top5[x.data.game_variation] || 0)+1;
-              break;
-            case 6:
-              stats.positions.top6++;
-              winRateGalaxie.top6[x.data.game_variation] = (winRateGalaxie.top6[x.data.game_variation] || 0)+1;
-              break;
-            case 7:
-              stats.positions.top7++;
-              winRateGalaxie.top7[x.data.game_variation] = (winRateGalaxie.top7[x.data.game_variation] || 0)+1;
-              break;
-            default:
-              stats.positions.top8++;
-              winRateGalaxie.top8[x.data.game_variation] = (winRateGalaxie.top8[x.data.game_variation] || 0)+1;
-          }
-
-          if(x.data.placement < 5) {
-            countsTop4[x.data.game_variation] = (countsTop4[x.data.game_variation] || 0)+1;
-            for (const t of x.data.units) {
-              unitsArray.push(t);
-            }
-            for (const s of x.data.traits) {
-              if (s.style > 0) {
-                traitsArrayT4.push(s);
+              for (const s of x.data.units) {
+                unitsArrayTotal.push(s);
               }
-              if(s.style === 1) {
-                traitsArrayT4S1.push(s);
-              } else if (s.style === 2) {
-                traitsArrayT4S2.push(s);
-              } else if (s.style === 3) {
-                traitsArrayT4S3.push(s);
+              for (const s of x.data.traits) {
+                if (s.style > 0) {
+                  traitsArrayTotal.push(s);
+                }
               }
+
+              counts[x.data.game_variation] = (counts[x.data.game_variation] || 0)+1;
+            };
+            for (let key in winRateGalaxie.top) {
+              w.push({key: key, value: winRateGalaxie.top[key]});
             }
-          }
-          for (const s of x.data.units) {
-            unitsArrayTotal.push(s);
-          }
-          for (const s of x.data.traits) {
-            if (s.style > 0) {
-              traitsArrayTotal.push(s);
+            winRateGalaxie.top = w;
+            for (const z of unitsArrayTop1) {
+              champCountTop1[z.character_id] = (champCountTop1[z.character_id] || 0)+1;
             }
-          }
+            unitsArrayTop1 = [];
+            for (const key of Object.keys(champCountTop1)) {
+              const value = champCountTop1[key];
+              unitsArrayTop1.push({x: key, y: value});
+            }
+            for (const c of unitsArray) {
+              champCount[c.character_id] = (champCount[c.character_id] || 0)+1;
+            }
+            unitsArray = [];
+            for (const key1 of Object.keys(champCount)) {
+              const value1 = champCount[key1];
+              unitsArray.push({x: key1, y: value1});
+            }
+            for (const y of unitsArrayTotal) {
+              champCountTotal[y.character_id] = (champCountTotal[y.character_id] || 0)+1;
+            }
+            unitsArrayTop1.sort((a, b) => b.y - a.y).splice(10, unitsArrayTop1.length - 10);
+            unitsArray.sort((a, b) => b.y - a.y).splice(10, unitsArray.length - 10);
 
-          counts[x.data.game_variation] = (counts[x.data.game_variation] || 0)+1;
-        };
-        for (let key in winRateGalaxie.top) {
-          w.push({key: key, value: winRateGalaxie.top[key]});
-        }
-        winRateGalaxie.top = w;
-        for (const z of unitsArrayTop1) {
-          champCountTop1[z.character_id] = (champCountTop1[z.character_id] || 0)+1;
-        }
-        unitsArrayTop1 = [];
-        for (const key of Object.keys(champCountTop1)) {
-          const value = champCountTop1[key];
-          unitsArrayTop1.push({x: key, y: value});
-        }
-        for (const c of unitsArray) {
-          champCount[c.character_id] = (champCount[c.character_id] || 0)+1;
-        }
-        unitsArray = [];
-        for (const key1 of Object.keys(champCount)) {
-          const value1 = champCount[key1];
-          unitsArray.push({x: key1, y: value1});
-        }
-        for (const y of unitsArrayTotal) {
-          champCountTotal[y.character_id] = (champCountTotal[y.character_id] || 0)+1;
-        }
-        unitsArrayTop1.sort((a, b) => b.y - a.y).splice(10, unitsArrayTop1.length - 10);
-        unitsArray.sort((a, b) => b.y - a.y).splice(10, unitsArray.length - 10);
+            for (const z of traitsArrayT1) {
+              countsT1[z.name] = (countsT1[z.name] || 0)+1;
+            }
+            traitsArrayT1 = [];
+            for (const key of Object.keys(countsT1)) {
+              const value = countsT1[key];
+              traitsArrayT1.push({x: key, y: value});
+            }
+            for (const z of traitsArrayT4) {
+              countsT4[z.name] = (countsT4[z.name] || 0)+1;
+            }
+            traitsArrayT4 = [];
+            for (const key of Object.keys(countsT4)) {
+              const value = countsT4[key];
+              traitsArrayT4.push({x: key, y: value});
+            }
+            for (const y of traitsArrayTotal) {
+              countsTotal[y.name] = (countsTotal[y.name] || 0)+1;
+            }
+            for (const y of traitsArrayT4S1) {
+              countsT4S1[y.name] = (countsT4S1[y.name] || 0)+1;
+            }
+            for (const y of traitsArrayT4S2) {
+              countsT4S2[y.name] = (countsT4S2[y.name] || 0)+1;
+            }
+            for (const y of traitsArrayT4S3) {
+              countsT4S3[y.name] = (countsT4S3[y.name] || 0)+1;
+            }
+            traitsArrayT1.sort((a, b) => b.y - a.y).splice(10, traitsArrayT1.length - 10);
+            traitsArrayT4.sort((a, b) => b.y - a.y).splice(10, traitsArrayT4.length - 10);
 
-        for (const z of traitsArrayT1) {
-          countsT1[z.name] = (countsT1[z.name] || 0)+1;
+            stats.count = {top4: countsTop4, perGalaxie: winRateGalaxie, total: counts, champs: { top1: unitsArrayTop1, top4: unitsArray, top4C: champCount, totalC: champCountTotal }, traits: { top1: traitsArrayT1, top4: traitsArrayT4, top4C: countsT4, totalC: countsTotal, top4S1: countsT4S1, top4S2: countsT4S2, top4S3: countsT4S3 }};
+            return stats;
+        } catch (err) {
+            this.setStatus(204);
+            console.error('Caught error', err);
         }
-        traitsArrayT1 = [];
-        for (const key of Object.keys(countsT1)) {
-          const value = countsT1[key];
-          traitsArrayT1.push({x: key, y: value});
-        }
-        for (const z of traitsArrayT4) {
-          countsT4[z.name] = (countsT4[z.name] || 0)+1;
-        }
-        traitsArrayT4 = [];
-        for (const key of Object.keys(countsT4)) {
-          const value = countsT4[key];
-          traitsArrayT4.push({x: key, y: value});
-        }
-        for (const y of traitsArrayTotal) {
-          countsTotal[y.name] = (countsTotal[y.name] || 0)+1;
-        }
-        for (const y of traitsArrayT4S1) {
-          countsT4S1[y.name] = (countsT4S1[y.name] || 0)+1;
-        }
-        for (const y of traitsArrayT4S2) {
-          countsT4S2[y.name] = (countsT4S2[y.name] || 0)+1;
-        }
-        for (const y of traitsArrayT4S3) {
-          countsT4S3[y.name] = (countsT4S3[y.name] || 0)+1;
-        }
-        traitsArrayT1.sort((a, b) => b.y - a.y).splice(10, traitsArrayT1.length - 10);
-        traitsArrayT4.sort((a, b) => b.y - a.y).splice(10, traitsArrayT4.length - 10);
-
-        stats.count = {top4: countsTop4, perGalaxie: winRateGalaxie, total: counts, champs: { top1: unitsArrayTop1, top4: unitsArray, top4C: champCount, totalC: champCountTotal }, traits: { top1: traitsArrayT1, top4: traitsArrayT4, top4C: countsT4, totalC: countsTotal, top4S1: countsT4S1, top4S2: countsT4S2, top4S3: countsT4S3 }};
-        return stats;
     }
 
     @Get('/summoners/entries/{id}')
@@ -383,6 +388,7 @@ export class summonersController extends Controller {
               if(!match) {
                 let sumE = new summonersEntriesModel({
                   userId: sum.userId,
+                  sumId: sum.id,
                   entrie: value
                 });
                 sumE.save();
@@ -398,9 +404,9 @@ export class summonersController extends Controller {
     }
 
     @Post('/summoners/match_info_ext')
-    public async setLastMatchInfoExt(@BodyProp('userId') userId: string ) {
+    public async setLastMatchInfoExt(@BodyProp('userId') userId: string, @BodyProp('sumId') sumId: string ) {
         this.getMatchesExt(userId);
-        let sum: ISummonerEntries[] = await summonersEntriesModel.find({userId: userId});
+        let sum: ISummonerEntries[] = await summonersEntriesModel.find({userId: userId, sumId: sumId});
         let data: any[];
         try {
           sum.forEach(ent => {
@@ -411,6 +417,7 @@ export class summonersController extends Controller {
                   data = res;
                   let sumE = new summonersMatchesModel({
                     userId: ent.userId,
+                    sumId: ent.sumId,
                     entrie: ent.entrie,
                     data: res
                   });
