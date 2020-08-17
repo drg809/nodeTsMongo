ad7198f0d3c90fe7af4981bbe6c68c6650b23540
Daniel Rodriguez <drg809@gmail.com>
2020-08-17 06:31:14 +0200

    summoner Id added to entries

--- src/models/summonerEntries.ts
+++ src/models/summonerEntries.ts
@@ -3,11 +3,13 @@ import * as mongoose from "mongoose";
 const summonersEntriesSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId, required: false },
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    sumId: { type: mongoose.Schema.Types.ObjectId, required: true },
     entrie: { type: String, unique : true, required : true, dropDups: true }
 });
 
 export interface ISummonerEntries extends mongoose.Document {
     userId?: string,
+    sumId?: string,
     entrie?: string
 };
 
